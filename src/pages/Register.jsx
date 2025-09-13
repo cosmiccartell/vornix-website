468c090e32051690a0f223329d1c55d6ad5a3ce4
cosmiccartell <cosmiccartell@gmail.com>
2025-09-13 22:40:18 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import config from '../config'; // Import the config
 import { authApi } from '../api/authApi';
 
 const Register = () => {
