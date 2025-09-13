cb4bd75170c1d39ec284f3ccd94aceac21018237
cosmiccartell <cosmiccartell@gmail.com>
2025-09-13 21:58:10 +0530

    Delete src/config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// This file is no longer needed for the API base URL,
-// as the proxy handles it automatically.
-// Leaving it here in case you want to add other frontend configurations later.
-export const config = {
-  // apiBaseUrl is now handled by the vite.config.js proxy.
-};
-
-export default config;
