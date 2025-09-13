34276872cf8963361dfe96b1685f9c0efe8ea1e8
cosmiccartell <cosmiccartell@gmail.com>
2025-09-13 22:32:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'  // Make sure it includes the .jsx extension
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HelmetProvider } from 'react-helmet-async'; // Import the new manager
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider> {/* Add the manager here, wrapping your App */}
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 )
